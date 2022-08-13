b23b10d5d9b429c5db2b73417734b9a4da07f3f0
Elvis Leite da SIlva <38222579+elvis7t@users.noreply.github.com>
2022-08-13 12:40:09 -0300

    Update init-database.sh

--- db/sql/init-database.sh
+++ db/sql/init-database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mysql -u root -proot useacabeca < "/docker-entrypoint-initdb.d/000-create-databases.sql"
+mysql -u root -proot useacabeca < "/docker-entrypoint-initdb.d/001-create-databases.sql"
 # mysql -u root -proot useacabeca < "/docker-entrepoint-initdb.d/001-create-tables.sql"
